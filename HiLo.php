64f72ee4d0f303a7b60de4eab1c71e93e6826437
Genaro Garza <genarogarza@MyMacbook.local>
2014-05-12 13:41:46 -0500

    final copy

--- HiLo.php
+++ HiLo.php
@@ -1,29 +1,34 @@
 <?php
 
 
-// gets number to guess
+// generates number to guess
 
 $x = rand(0,100);
-echo "{$x}\n";
+//echo "{$x}\n";
 
+$guesses = 0;
 
-// evaluates users guess
+// receives users guess
 fwrite(STDOUT, "guess a number betwen 0 and 100\n");
 
-// echo if correct
+// responses
 
 do {
 
 	$guess = fgets(STDIN);
-
+	// add 1 to #guesses
+	$guesses++;
 	if ($guess == $x) {
 		echo "Correctamundo !!\n";
+		echo "it only took you {$guesses} times to guess!\n\n\n\n";
 	} elseif ($guess > $x) {
 		echo " Lower\n";
 	} else {
 		echo "Higher\n";
 	}
 
+
+
+// loop if wrong
 } while ($guess != $x);
 
-// final output
